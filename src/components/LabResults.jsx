c90b63ce335624d185856e7d9cfb9a35212a1d12
MhideTech <okefolahanolamide2006@gmail.com>
2024-05-30 10:16:33 +0100

    Added chart.js package

--- src/components/LabResults.jsx
+++ src/components/LabResults.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable react/prop-types */
 import styles from "./LabResults.module.css";
 
-function LabResults() {
+function LabResults({ selectedPatient }) {
   return (
     <div className={styles.labResults}>
       <h1>Lab Results</h1>
       <div className={styles.resultContainer}>
+        {selectedPatient?.lab_results?.map((result) => {
+          <div className={styles.result}>
+            <p>{result}</p>
+            <img src="/src/assets/download-icon.png" alt="" />
+          </div>;
+        })}
         <div className={styles.result}>
           <p>Blood Tests</p>
           <img src="/src/assets/download-icon.png" alt="" />
